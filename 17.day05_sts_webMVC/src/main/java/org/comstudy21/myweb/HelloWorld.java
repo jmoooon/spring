e71986a517ace4eff9027087670cc94eceb1760f
Jeongryeol Lee <duxbellorn@gmail.com>
2018-06-16 11:15:38 +0900

    init

---
+++ 17.day05_sts_webMVC/src/main/java/org/comstudy21/myweb/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.comstudy21.myweb;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+	}
+
+}
